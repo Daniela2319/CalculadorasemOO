0cd9a1852a34f2e231cbc6b6143f0c939f8460dd
Daniela2319 <frankvt04@gmail.com>
2022-08-26 20:51:36 -0300

    First

---
+++ src/CalculadoraO/CalculadoraO.java
@@ -0,0 +1,32 @@
+package CalculadoraO;
+
+// A linguagem java contém pacotes com classe prontas (código já escrito, uma biblioteca)
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// Declarando a Classe
+public class CalculadoraO {
+
+    // A função (Método) Principal - main
+    // Void indica que este método não retornará um valor
+    public static String main(String[] args) throws IOException {
+
+        System.out.println("Calculador sem OO");
+        char ch = ' ';
+        while (true){
+            System.out.println("Para sair digite S ou s:  ");
+
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
